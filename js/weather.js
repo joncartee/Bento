e6e55814b538edecc11ad54138fd63bcd89b5840
Jon Cartee <jcartee@bitstobytes.org>
2021-01-27 00:21:18 -0800

    added personal api key

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'c47af3f6a7c61d32153da83b79429d22';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 40.453011;
+  let longitude = -122.298523;
 
   getWeather(latitude, longitude);
 }
